b94541744ac9c3ac48f512c7bdff9044f70a54ef
MRPhillip007 <filipplyamov@gmail.com>
2022-07-31 14:42:21 +0300

    Greetings functionality

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// Main functionality 
 Console.WriteLine("Hello, nice to meet you :) ");
+
+
+// Another developer's work 
+Console.WriteLine("Enter your name: ");
+string name = Console.ReadLine();
+
+if (!string.IsNullOrWhiteSpace(name))
+{
+    Console.WriteLine("{0}, welcome back! ", name);
+}
+else
+{
+    Console.WriteLine("Please, enter valid name... ");
+}
+
+Console.ReadLine();
